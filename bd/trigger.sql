1d55502140193c8b6138ee1f7aa8c5ab7dfc0ca8
mike trujillo <38016591+mike121986@users.noreply.github.com>
2022-03-10 12:09:59 -0600

    editar el trigger historico pedido

--- bd/trigger.sql
+++ bd/trigger.sql
@@ -1,9 +1,9 @@
 DELIMITER //
-CREATE TRIGGER historico_pedido AFTER INSERT ON pedidos
+ALTER TRIGGER historico_pedido AFTER INSERT ON pedidos
 FOR EACH ROW 
 	BEGIN 
 		INSERT INTO historico_pedidos
-	(idPeddios, idnotaPedido, idUsuarioPedido, idClientePedido, idProductoPedido, pzProductoPedido, fechaAltaProductoPedido, statusProductoPedido)
-	VALUES (NEW.idPeddios, NEW.idnotaPedido, NEW.idUsuarioPedido, NEW.idClientePedido, NEW.idProductoPedido, NEW.pzProductoPedido, NEW.fechaAltaProductoPedido, NEW.statusProductoPedido);
+	(idPeddios, idnotaPedido, idUsuarioPedido, idClientePedido, idProductoPedido, pzProductoPedido, fechaAltaProductoPedido, statusProductoPedido,fechaEntregaPedido)
+	VALUES (NEW.idPeddios, NEW.idnotaPedido, NEW.idUsuarioPedido, NEW.idClientePedido, NEW.idProductoPedido, NEW.pzProductoPedido, NEW.fechaAltaProductoPedido, NEW.statusProductoPedido,NEW.fechaEntregaPedido);
 	END;
 //
